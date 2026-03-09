18c4322021fae3005587099392b7b0df5d356a73
SimchenkovM <simchenkov_m@mineframe.ru>
2026-03-09 13:18:54 +0300

    refactor: use lowercase 'events' route for REST consistency

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EventGrok.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("events")]
 public class EventsController(IEventService eventService) : ControllerBase
 {
     [HttpGet]
